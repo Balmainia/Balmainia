c74868a3d0e53b9d8cc26b18eb52d0a6d2624283
Your Name <you@example.com>
2019-10-22 14:10:13 +0200

    22-10-2019 14:10

--- app/src/main/java/com/example/balmainia/MainActivity.kt
+++ app/src/main/java/com/example/balmainia/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.balmainia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun beginSpel(view: View) {
+
+       val intent = Intent(this, begin_spel::class.java).apply {
+        }
+        startActivity(intent)
+
     }
 }
---
+++ app/src/main/java/com/example/balmainia/begin_spel.kt
@@ -0,0 +1,12 @@
+package com.example.balmainia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class begin_spel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_begin_spel)
+    }
+}
